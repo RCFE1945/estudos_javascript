2eea600618e1f11f238e09ece72e3a6e1462db2f
Ramon Coelho De Souza <57835065+RCFE1945@users.noreply.github.com>
2022-02-12 07:48:37 -0300

    novo teste

--- codigos_teste/teste.js
+++ codigos_teste/teste.js
@@ -1,3 +1,21 @@
+//Algotimo recursivo
+/*
+function findSolution(target){
+    function find(start,history){
+        if(start == target){
+            return history;
+        }else if(start > target){
+            return null;    
+        }else{
+            return find(start+5,"("+history+"+5)") || find(start*3,"("+history+"*3)");
+        }
+    }
+    return find(1,"1");
+}
+console.log(findSolution(24));
+// (((1 * 3) + 5) * 3)
+*/
+
 //Testando a prioridade de argumentos quando e passado mais argumentos que necessario
 /*
 alert("Hello World", "Good Morning", "Good Evening", "Good Nigth");
